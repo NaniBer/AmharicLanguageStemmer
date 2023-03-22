294f4ba4b4bdcd5eafcf204b7c8740cf5e20fd86
boyobombayo <romhayyzbello@gmail.com>
2023-03-22 21:58:23 +0300

    -Rename files and variables -Add missed letters (fidels) -Add a preposition

--- prepositionSuffixes.py
+++ prepositionSuffixes.py
@@ -1,9 +1,9 @@
 word = input("ቃላትን ያስገቡ፡ ")
 FirstcharOne = word[0]
-    # print(FirstcharOne)
-possesionSuffixes = ["የ", "በ", "ከ"]
-if FirstcharOne in possesionSuffixes:
-     word = word[1:]
-     print(word)
+# print(FirstcharOne)
+prepositionSuffixes = ["የ", "በ", "ከ", "ለ"]
+if FirstcharOne in prepositionSuffixes:
+    word = word[1:]
+    print(word)
 else:
-     print(word)
+    print(word)
