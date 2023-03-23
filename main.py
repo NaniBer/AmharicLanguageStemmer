60e7caeacedaa4997ceec4fc5924629f3097775d
NaniBer <gdgr2010@gmail.com>
2023-03-23 14:35:41 +0300

    Final changes done

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-import possesionSuffixes
+import possessionSuffixes
 import prepositionSuffixes
 import genderSuffixes
 import pluralSuffixes
 
 
 
-# word = input("ቃላትን ያስገቡ፡ ")
-word = "ስለሃገራችን"
+word = input("ቃላትን ያስገቡ፡ ")
+
 lastcharOne = word[-1]
 lastcharTwo = word[-2:]
 
 #Steps Preposition -> possesion -> gender -> plural
 word=prepositionSuffixes.PrepositionSuffix(word)
-word=possesionSuffixes.PossessionSuffixes(word)
+word=possessionSuffixes.PossessionSuffixes(word)
 word=genderSuffixes.GenderSuffixes(word)
 word=pluralSuffixes.PluralSuffixes(word)
+
 
 print(word)
 
