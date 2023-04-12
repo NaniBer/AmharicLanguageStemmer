1534a522f46906e9112cf30c2fbf8760b8eccc57
NaniBer <gdgr2010@gmail.com>
2023-04-12 11:46:13 +0300

    Made few changes to dictionart file

--- main.py
+++ main.py
@@ -2,22 +2,40 @@ import possessionSuffixes
 import prepositionSuffixes
 import genderSuffixes
 import pluralSuffixes
+import dictionary
 
 
+# word = input("ቃላትን ያስገቡ፡ ")
 
-word = input("ቃላትን ያስገቡ፡ ")
+word = "በየምክንያቶች"
 
-lastcharOne = word[-1]
-lastcharTwo = word[-2:]
 
-#Steps Preposition -> possesion -> gender -> plural
-word=prepositionSuffixes.PrepositionSuffix(word)
-word=possessionSuffixes.PossessionSuffixes(word)
-word=genderSuffixes.GenderSuffixes(word)
-word=pluralSuffixes.PluralSuffixes(word)
+# Check if the word is a stem word
+
+
+while word not in dictionary.stemWord:
+
+    lastcharOne = word[-1]
+    lastcharTwo = word[-2:]
+
+#     # Steps Preposition -> possesion -> gender -> plural
+
+    word = possessionSuffixes.PossessionSuffixes(word)
+    print(word)
+    if (word in dictionary.stemWord):
+        break
+    word = genderSuffixes.GenderSuffixes(word)
+    print(word)
+    if (word in dictionary.stemWord):
+        break
+    word = pluralSuffixes.PluralSuffixes(word)
+    print(word)
+    if (word in dictionary.stemWord):
+        break
+    word = prepositionSuffixes.PrepositionSuffix(word)
 
 
 print(word)
 
 
-
+print(word)
--- possessionSuffixes.py
+++ possessionSuffixes.py
@@ -1,32 +1,28 @@
 import letterDefinitions
 
+
 def PossessionSuffixes(word):
     lastword = word[-1]
     lastthreewords = word[-3:]
-    lasttwowords= word[-2:]
+    lasttwowords = word[-2:]
 
-
-    # print(lastword)
     if lastword in letterDefinitions.possessionWords1:
         word = word[:1]
-
 
     elif lastthreewords in letterDefinitions.possessionWords2:
         word = word[:-3]
 
     elif lasttwowords in letterDefinitions.possessionWords5:
         word = word[:-2]
-        index= letterDefinitions.possessionword6.index(word[-1])
-        word= word.replace(word[-1], letterDefinitions.sixthLetter[index])
-
+        index = letterDefinitions.possessionword6.index(word[-1])
+        word = word.replace(word[-1], letterDefinitions.sixthLetter[index])
 
     elif lastword in letterDefinitions.possessionWord3:
         index = letterDefinitions.possessionWord3.index(word[-1])
         word = word.replace(word[-1], letterDefinitions.sixthLetter[index])
-            
 
     elif lastword in letterDefinitions.possessionWords4:
         index1 = letterDefinitions.possessionWords4.index(word[-1])
         word = word.replace(word[-1], letterDefinitions.sixthLetter[index1])
-            
+
     return word
